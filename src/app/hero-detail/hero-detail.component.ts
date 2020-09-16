58d4a7ba383452b66e26668ac6ff4b2dd9427ca0
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-09-16 18:22:46 +0300

    Tutorial completed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,6 +23,10 @@ export class HeroDetailComponent implements OnInit {
     this.getHero();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
+
   goBack(): void {
     this.location.back();
   }
